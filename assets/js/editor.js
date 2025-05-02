bf4e646442ecc234dd51c625ba95a0a27ddf4f93
mstuyven <mirostuyven@gmail.com>
2025-05-03 00:57:42 +0200

    Maak externe links bewerkbaar

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -3,10 +3,11 @@ const EDITING_KEY = 'chirosintanna-editing'
 const REPO = 'chirosintanna/chirosintanna.be'
 
 const editorButton = document.querySelector('footer .editor')
+if (editorButton) {
+  initialize()
+}
 
-main()
-
-function main() {
+function initialize() {
   if (localStorage.getItem(TOKEN_KEY)) {
     editorButton.textContent = 'Bewerken'
   } else {
@@ -16,12 +17,11 @@ function main() {
     enableEditor()
   }
 
-  editorButton?.addEventListener('click', () => {
+  editorButton.addEventListener('click', () => {
     if (sessionStorage.getItem(EDITING_KEY)) {
       disableEditor()
       return
     }
-
     if (localStorage.getItem(TOKEN_KEY) === null) {
       const token = prompt('Inloggen met github_pat')
       if (token && token.startsWith('github_pat_')) {
@@ -31,7 +31,6 @@ function main() {
         return
       }
     }
-
     enableEditor()
   })
 }
@@ -46,8 +45,11 @@ function enableEditor() {
         return
       }
       evt.preventDefault()
-      console.log('CLICK', e.getAttribute('data-edit'))
-      // TODO
+      makeEdit(e.getAttribute('data-edit'))
+        .then((success) => {
+          if (success) alert('Succesvol bewerkt! Aanpassing is publiek binnen enkele seconden...')
+        })
+        .catch((e) => alert(e instanceof Error ? e.message : `${e}`))
     })
   })
 }
@@ -56,4 +58,61 @@ function disableEditor() {
   sessionStorage.removeItem(EDITING_KEY)
   document.body.classList.remove('editing')
   editorButton.textContent = 'Bewerken'
+}
+
+async function makeEdit(key) {
+  // 1. Huidige broncode ophalen van GitHub
+  const path = `${location.pathname.replace(/^\//, '')}index.html`
+  const getRes = await fetch(`https://api.github.com/repos/${REPO}/contents/${path}`, {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
+    },
+  })
+  const getData = await getRes.json()
+  if (!getRes.ok) {
+    throw new Error(`Kan pagina niet bewerken: ${getRes.status} ${getData.message}`)
+  }
+  const text = decodeURIComponent(escape(atob(getData.content)))
+  const parser = new DOMParser()
+  const html = parser.parseFromString(text, 'text/html')
+  const element = html.querySelector(`[data-edit=${key}]`)
+
+  // 2. Element aanpassen in de DOM
+  if (element instanceof HTMLAnchorElement && element.href.startsWith('https://')) {
+    const newLink = prompt('Link aanpassen', element.href)
+    if (!newLink || element.href === newLink) {
+      return false
+    }
+    if (!newLink.startsWith('https://')) {
+      throw new Error('Link moet beginnen met https://')
+    }
+    element.href = newLink
+  } else {
+    throw new Error('Kan dit element niet bewerken')
+  }
+
+  // 3. Nieuwe broncode committen naar GitHub
+  const documentText = html.documentElement.outerHTML
+    .replace('<html lang="nl"><head>', '<html lang="nl">\n<head>')
+    .replace('</body></html>', '</body>\n</html>')
+    .replace(/\n+<\/body>/, '\n</body>')
+  const newText = `<!DOCTYPE html>\n${documentText}\n`
+  const newTextUtf8 = unescape(encodeURIComponent(newText))
+  const putRes = await fetch (`https://api.github.com/repos/${REPO}/contents/${path}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: `🤖 Bewerk ${key}`,
+      content: btoa(newTextUtf8),
+      sha: getData.sha,
+    })
+  })
+  const putData = await putRes.json()
+  if (!putRes.ok) {
+    throw new Error(`Fout tijdens het aanpassen: ${putRes.status} ${putData.message}`)
+  }
+  return true
 }
